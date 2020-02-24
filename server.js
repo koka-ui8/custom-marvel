f380b726f40e58831b575a0572e8f588b0677122
koka-ui8 <60244663+koka-ui8@users.noreply.github.com>
2020-02-23 21:10:14 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const marvelCharacters = require('./MarvelCharacters.json');
 // Used express module to implement the API's
 const app = express();
 app.use(express.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://custom-coding-exercise.herokuapp.com"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "https://custom-coding-exercise.herokuapp.com"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
  
 //READ Request Handlers
 app.get('/', (req, res) => {
